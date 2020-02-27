c3d6a6aedf9b545ccd0215049b17b4b69fefb88c
Jonathan <jonathan@foreningshuset.se>
2020-02-27 16:20:04 +0100

    Publish to gh-pages

---
+++ docs/precache-manifest.6f5135354a6d38faf271574473633723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cd17302827c63143921e75be1d2298",
+    "url": "/cocoberry/index.html"
+  },
+  {
+    "revision": "172a3682c86331311867",
+    "url": "/cocoberry/static/css/2.c6250e3a.chunk.css"
+  },
+  {
+    "revision": "f4598464783883e437bd",
+    "url": "/cocoberry/static/css/main.5b27bfbe.chunk.css"
+  },
+  {
+    "revision": "172a3682c86331311867",
+    "url": "/cocoberry/static/js/2.e18a04d8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cocoberry/static/js/2.e18a04d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4598464783883e437bd",
+    "url": "/cocoberry/static/js/main.d23e65fe.chunk.js"
+  },
+  {
+    "revision": "632dbd3a2741ac1e6b24",
+    "url": "/cocoberry/static/js/runtime-main.831a34fe.js"
+  }
+]);
