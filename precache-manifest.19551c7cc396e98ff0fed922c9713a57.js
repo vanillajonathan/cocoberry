1de98b380b88aacd4ff595ae79fd816c69ad08b7
gg <g>
2020-06-22 16:54:04 +0200

    Updates

---
+++ precache-manifest.19551c7cc396e98ff0fed922c9713a57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6858b583b3b165683d1e73d51401b341",
+    "url": "/cocoberry/index.html"
+  },
+  {
+    "revision": "75076636246b20bbf6f9",
+    "url": "/cocoberry/static/css/2.d805e148.chunk.css"
+  },
+  {
+    "revision": "85077bafec0bd167ba54",
+    "url": "/cocoberry/static/css/main.5b27bfbe.chunk.css"
+  },
+  {
+    "revision": "75076636246b20bbf6f9",
+    "url": "/cocoberry/static/js/2.50f54c8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cocoberry/static/js/2.50f54c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85077bafec0bd167ba54",
+    "url": "/cocoberry/static/js/main.082f63d5.chunk.js"
+  },
+  {
+    "revision": "ff3663ae766b909607dc",
+    "url": "/cocoberry/static/js/runtime-main.d98fcd03.js"
+  }
+]);
