1b081f2c1eb1d20c36e681cbd767a17c0799cefc
Jonathan <jonathan@foreningshuset.se>
2021-03-30 11:51:34 +0200

    Fix UTF-8 BOM

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-﻿import * as React from "react";
+import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
